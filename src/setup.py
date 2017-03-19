ccd591a8fb9e370f03330e7f5d0e94abb249e077
Anil Kushwah <akushwah95@gmail.com>
2017-03-19 16:26:23 +0530

    Added library and example scripts

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name            = 'pyFingerprint',
+    version         = '1.0',
+    description     = 'Python written library for project.',
+    author          = 'Anil Kushwah',
+    author_email    = 'akushwah29@gmail.com',
+    url             = 'None',
+    license         = 'None',
+    package_dir     = {'': 'files'},
+    packages        = ['pyfingerprint'],
+)
