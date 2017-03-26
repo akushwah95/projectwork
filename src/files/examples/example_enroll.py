2409de65d63f59e5227819f4c880e2b442dd3d2d
rahul_windows <kushwahrahul129@gmail.com>
2017-03-26 22:52:53 +0530

    Support

--- src/files/examples/example_enroll.py
+++ src/files/examples/example_enroll.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 
 """
+Enrolls a voter to the database
 """
+
 import requests
 import time
 from pyfingerprint.pyfingerprint import PyFingerprint
 import hashlib
 from platform import platform
 
-## Enrolls new finger
-##
-plt= platform().lower()
+plt = platform().lower()
 if "windows" in plt:
-    port="COM11"
+    port = "COM11"
 else:
-    port="/dev/ttyUSB0"
-post_url="http://35.154.44.42:8080/userenrollment/webapi/users"
-## Tries to initialize the sensor
+    port = "/dev/ttyUSB0"
+
+post_url = "http://35.154.44.42:8080/userenrollment/webapi/users"
+
 try:
     f = PyFingerprint(port, 57600, 0xFFFFFFFF, 0x00000000)
 
@@ -29,12 +30,13 @@ except Exception as e:
     print('Exception message: ' + str(e))
     exit(1)
 
-## Gets some sensor information
-print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+#print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
 
-## Tries to enroll new finger
+
 try:
-    print('Waiting for finger...')
+    print ('-------------------------------')
+    print ('Welcome To Enrollment Portal !!!')
+    print ('Waiting for finger to Enroll')
 
     ## Wait that finger is read
     while ( f.readImage() == False ):
@@ -48,64 +50,54 @@ try:
     positionNumber = result[0]
 
     if ( positionNumber >= 0 ):
-        print('Template already exists at position #' + str(positionNumber))
+        #print('Template already exists at position #' + str(positionNumber))
+        print ('Fingerprint is already stored in the Database !!')
+        characterics = str(f.downloadCharacteristics(0x01))
+        hashes = hashlib.sha256(characterics).hexdigest()
+        get_utl=post_url+"/"+hashes
+        resp = requests.get(get_utl)
+        print (resp.text)
+        time.sleep(5)
+        print ('Exiting now !!!')
+        time.sleep(2)
         exit(0)
     print('Remove finger...')
-    time.sleep(2)
+    time.sleep(5)
 
     print('Waiting for same finger again...')
 
-    ## Wait that finger is read again
     while ( f.readImage() == False ):
         pass
 
-    ## Converts read image to characteristics and stores it in charbuffer 2
     f.convertImage(0x02)
 
-    ## Compares the charbuffers and creates a template
     if f.compareCharacteristics() != 0:
         f.createTemplate()
-        ## Saves template at new position number
         characterics = str(f.downloadCharacteristics(0x01))
-        print(characterics)
-        fl = open("shas-store", 'a')
-        fr = open("shas-store", 'r')
-        ## Hashes characteristics of template
         hashes = hashlib.sha256(characterics).hexdigest()
-        print type(hashes)
-        hashes_file = fr.read()
-        if hashes not in hashes_file:
-            fl.write(hashes + '-')
-        else:
-            print "Hash already present machaaa!!!"
-        print('SHA-2 hash of template: ' + hashlib.sha256(characterics).hexdigest())
-
+        print ('Please Fill in the details that follows to Enroll New Voter : ')
         voter_name = raw_input("Please enter you name : ")
         dob = raw_input("Please Enter Date of Birth : ")
         mother_name = raw_input("Please Enter Mother's Name : ")
         father_name = raw_input("Please Enter Father's name : ")
         address = raw_input("Please enter address :")
-        mobile_number = int(raw_input("Please Enter Mobile Number"))
-        headers= {'content-type': 'application/json'}
-        diction="{\"address\": \"%s\",\"dob\":\"%s\",\"fatherName\": \"%s\",\"fingerPrint\": \"%s\",\"mobileNumber\": %s, \
-                \"motherName\": \"%s\",\"name\": \"%s\"}" % (address,dob,father_name,hashes,mobile_number,mother_name,voter_name)
-        cc = requests.post(post_url,data=diction,headers=headers)
-        print cc.text
-
-        cd = requests.get(post_url)
-        print cd.text
-        print cd.content
+        mobile_number = int(raw_input("Please Enter Mobile Number :"))
         positionNumber = f.storeTemplate()
-        print('Finger enrolled successfully!')
-        print('New template position #' + str(positionNumber))
+        headers = {'content-type': 'application/json'}
+        diction = "{\"address\": \"%s\",\"dob\":\"%s\",\"fatherName\": \"%s\",\"fingerPrint\": \"%s\",\"mobileNumber\": %s,\
+                \"motherName\": \"%s\",\"name\": \"%s\"}" \
+                % (address, dob, father_name, hashes, mobile_number, mother_name, voter_name)
+        print ('Please be Patient while we store the data onto our database')
+        send_data = requests.post(post_url, data=diction, headers=headers)
+        if send_data.status_code == 200:
+            print ('Data sent successfully !!')
+        print ('Congratulations %s' % voter_name)
+        print ('Finger enrolled successfully!')
+        time.sleep(5)
+        #print('New template position #' + str(positionNumber))
     else:
         print('Fingerprints do not match')
-
-    ## Saves template at new position number
-    # positionNumber = f.storeTemplate()
-    # print('Finger enrolled successfully!')
-    # print('New template position #' + str(positionNumber))
-
+        time.sleep(5)
 except Exception as e:
     print('Operation failed!')
     print('Exception message: ' + str(e))
--- src/files/examples/example_search.py
+++ src/files/examples/example_search.py
@@ -5,19 +5,21 @@
 """
 
 import hashlib
+import os
 from platform import platform
+
+import time
 from pyfingerprint.pyfingerprint import PyFingerprint
 import requests
 
-## Search for a finger
-##
-url="http://35.154.44.42:8080/userenrollment/webapi/voter"
-plt= platform().lower()
-if "windows" in plt:
-    port="COM11"
-else:
-    port="/dev/ttyUSB0"
 
+url = "http://35.154.44.42:8080/userenrollment/webapi/voter"
+plt = platform().lower()
+if "windows" in plt:
+    port = "COM11"
+else:
+    port = "/dev/ttyUSB0"
+party_list = {1: "BJP", 2: "CONGRESS", 3: "AAP", 4: "LSP"}
 ## Tries to initialize the sensor
 try:
     f = PyFingerprint(port, 57600, 0xFFFFFFFF, 0x00000000)
@@ -28,15 +30,16 @@ try:
 except Exception as e:
     print('The fingerprint sensor could not be initialized!')
     print('Exception message: ' + str(e))
+    time.sleep(5)
     exit(1)
 
 ## Gets some sensor information
-print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+#print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
 
 ## Tries to search the finger and calculate hash
 try:
-    print('Waiting for finger...')
-
+    print('Waiting for finger to start voting :')
+    print('Please keep your finger on the sensor')
     ## Wait that finger is read
     while ( f.readImage() == False ):
         pass
@@ -46,17 +49,18 @@ try:
 
     ## Searchs template
     result = f.searchTemplate()
-
     positionNumber = result[0]
     accuracyScore = result[1]
 
     if ( positionNumber == -1 ):
-        print('No match found!')
+        print('No match found for this fingerprint')
+        print ('Exiting')
+        time.sleep(5)
         exit(0)
     else:
-        print('Found template at position #' + str(positionNumber))
-        print('The accuracy score is: ' + str(accuracyScore))
-
+        #print('Found template at position #' + str(positionNumber))
+        #print('The accuracy score is: ' + str(accuracyScore))
+        print ('Found you fingerprint!!')
     ## OPTIONAL stuff
     ##
 
@@ -65,28 +69,35 @@ try:
 
     ## Downloads the characteristics of template loaded in charbuffer 1
     characterics = str(f.downloadCharacteristics(0x01))
-    fl=open("shas",'a')
-    fr=open("shas",'r')
+
     ## Hashes characteristics of template
     hashes = hashlib.sha256(characterics).hexdigest()
-    print type(hashes)
-    hashes_file=fr.read()
-    if hashes not in hashes_file:
-    	fl.write(hashes+'-')
-    print('SHA-2 hash of template: ' + hashlib.sha256(characterics).hexdigest())
-    fl.close()
+
     get_url = url+"/"+hashes
-    cc=requests.get(get_url)
-    print cc.status_code
-    print cc.text
-    if cc.status_code==204:
-        vote_to=raw_input("Party name to vote to :")
-        post_url=url
-        headers={'content-type':'application/json'}
-        payload = "{\"fingerPrint\":\"%s\",\"hasVotedTo\":\"%s\"}" % (hashes, vote_to)
-        cc= requests.post(post_url, data=payload, headers=headers)
+    get_data = requests.get(get_url)
+    if get_data.status_code == 204:
+        print (get_data.text)
+        while True:
+            print ('List of parties along with the number :')
+            for number, party_name in party_list.iterkeys():
+                print party_name,number
+            try:
+                vote_to=int(raw_input("Please enter Party Number to vote to:"))
+                if party_list.get(vote_to) is None:
+                    raise ValueError
+                break
+            except ValueError as v:
+                print ('Invalid Party Number!!!')
+                print ('Try Again')
+                os.system('clear')
+        post_url = url
+        headers = {'content-type' : 'application/json'}
+        payload = "{\"fingerPrint\":\"%s\",\"hasVotedTo\":\"%s\"}" % (hashes, party_list.get(vote_to))
+        cc = requests.post(post_url, data=payload, headers=headers)
         print cc.text
+        time.sleep(5)
 except Exception as e:
     print('Operation failed!')
     print('Exception message: ' + str(e))
+    time.sleep(5)
     exit(1)
