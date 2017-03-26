40e5c87efcc2bab297758480faabb109463cb18e
rahul_windows <kushwahrahul129@gmail.com>
2017-03-26 23:38:50 +0530

    improv

--- src/files/examples/voting_results.py
+++ src/files/examples/voting_results.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import requests
+import time
+
 url="http://35.154.44.42:8080/userenrollment/webapi/voter/votecount"
 print ('Welcome to EC Portal ')
 print ('Please wait we are fetching the Voting Data')
@@ -8,3 +10,6 @@ voting_data = requests.get(url)
 print voting_data.text
 print ('-----------')
 print voting_data.content
+v=raw_input('Press any key to exit')
+print ('Exiting.....')
+time.sleep(5)
